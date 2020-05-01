f454b99ac8bb5c0e2d268799f2bae6c7ece81db8
Ismael Guimarães <56137836+ismaelguimaraes@users.noreply.github.com>
2020-04-30 22:00:42 -0300

    Add user creation migration

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'knex'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
