c407fbc0aad576dc8dc90398c057fc631f0b1e97
Ismael Guimarães <56137836+ismaelguimaraes@users.noreply.github.com>
2020-05-01 12:04:24 -0300

    Creation of the create route and middleware catch

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,6 @@ const UserController = require('./controllers/UserController');
 const routes = express.Router();
 
 routes.get('/users', UserController.index);
-
+routes.post('/users', UserController.create);
 
 module.exports = routes;
