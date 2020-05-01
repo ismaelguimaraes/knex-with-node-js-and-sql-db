c9397560bca5cfc7331a736d7cb4fa04435a8765
Ismael Guimarães <56137836+ismaelguimaraes@users.noreply.github.com>
2020-04-30 22:18:01 -0300

    Insert table user in the database

--- src/database/migrations/20200430215824_create_table_users.js
+++ src/database/migrations/20200430215824_create_table_users.js
@@ -1,8 +1,10 @@
 
-exports.up = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable('users', table => {
+    table.increments('id').primary();
+    table.text('username').unique().notNullable();
 
-exports.down = function(knex) {
-  
-};
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+});
+
+exports.down = knex => knex.schema.dropTable('users');
