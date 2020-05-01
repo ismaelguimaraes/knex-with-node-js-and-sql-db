f468e91a9f9e3a40a584288610097d1b548e9083
Ismael Guimarães <56137836+ismaelguimaraes@users.noreply.github.com>
2020-05-01 12:11:47 -0300

    Creation update route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -16,5 +16,17 @@ module.exports = {
         } catch(error) {
             next(error);
         }
+    },
+
+    async update(req, res, next) {
+        try {
+            const { username } = req.body;
+            const { id } = req.params;
+
+            await knex('users').update({ username }).where({ id });
+            return res.send();
+        } catch (error) {
+            next(error);
+        }
     }
 }
